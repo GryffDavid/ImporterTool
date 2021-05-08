e45a6e197db6d46e512c7deab491c9354e8a8b30
Gryff David <gryffdavid@gmail.com>
2021-05-08 19:26:21 +0200

    Got a quick test going to apply settings to a texture. Settings used are currently hardcoded.

---
+++ ImporterTool/Assets/Editor/AssetImporterTool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetImporterTool : MonoBehaviour
+{
+    [MenuItem("Assets/Apply Import Settings")]
+    static void ApplyImportSettings()
+    {
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+        TextureImporter importer = (TextureImporter)AssetImporter.GetAtPath(path);
+        importer.filterMode = FilterMode.Trilinear;
+        importer.maxTextureSize = 1024;
+
+        AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+    }
+}
