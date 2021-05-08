9104261d6152c138f8f2633ed7b9dc44e1e7a07d
Gryff David <gryffdavid@gmail.com>
2021-05-08 19:41:29 +0200

    Added a script that uses the AssetPostprocessor to automatically apply changes to .png files that are imported. Currently does not work with audio. Settings are still hardcoded.

---
+++ ImporterTool/Assets/Editor/ImportScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class ImportScript : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter importer = assetImporter as TextureImporter;
+        String name = importer.assetPath.ToLower();
+
+        if (name.Substring(name.Length - 4, 4) == ".png")
+        {
+            importer.filterMode = FilterMode.Bilinear;
+            importer.maxTextureSize = 1024;
+        }
+    }
+}
