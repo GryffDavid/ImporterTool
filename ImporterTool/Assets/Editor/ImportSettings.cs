29082b99ebd5c63cc995580dc290c18d2af23a72
Gryff David <gryffdavid@gmail.com>
2021-05-08 19:12:25 +0200

    Added a texture for testing late on. Created variables for the texture filter mode, audio sample rate and max texture size.

---
+++ ImporterTool/Assets/Editor/ImportSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "ImportSettings", menuName = "Import Settings", order = 1)]
+public class ImportSettings : ScriptableObject
+{
+    public FilterMode TetxureFilterMode;
+    public AudioSampleRateSetting AudioSampleRate;
+
+    public int MaxTextureSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
