98bb04df7e5d2059997c98bf5aeffa340171e305
Gryff David <gryffdavid@gmail.com>
2021-05-08 20:17:08 +0200

    Make sure that the ImportScript only applies settings automatically to newly imported files otherwise settings are just automatically updated and settings can't be set individually on assets, if necessary. Added a folder for sound effects. Added a sound effect for testing later.

--- ImporterTool/Assets/Editor/ImportScript.cs
+++ ImporterTool/Assets/Editor/ImportScript.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System;
 
 public class ImportScript : AssetPostprocessor
 {
     void OnPreprocessTexture()
     {
-        TextureImporter importer = assetImporter as TextureImporter;
-        importer.filterMode = FilterMode.Bilinear;
-        importer.maxTextureSize = 1024;
+        //I originally tried checking for a .meta file, but the OnPreprocessTexture runs after the .meta has been generated
+         
+        bool isNewFile = assetImporter.importSettingsMissing; //This returns true if there is no meta file with the asset.
+
+        if (isNewFile)
+        {
+            TextureImporter importer = assetImporter as TextureImporter;
+            importer.filterMode = FilterMode.Bilinear;
+            importer.maxTextureSize = 1024;
+        }
     }
 }
