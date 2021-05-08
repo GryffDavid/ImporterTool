cc29779e282fc9a9814fd6213271863808992491
Gryff David <gryffdavid@gmail.com>
2021-05-08 20:43:40 +0200

    Wrote a function to find Import Settings in the folder selected. The function gets the correct files, but the AssetDatabase.FindAssets function that Unity has does a search of the child folders too. So once the recursive search reaches the main Assets folder it returns completely irrelevant files. Looking for a way around it.

--- ImporterTool/Assets/Editor/AssetImporterTool.cs
+++ ImporterTool/Assets/Editor/AssetImporterTool.cs
@@ -10,6 +10,8 @@ public class AssetImporterTool : MonoBehaviour
     {
         string path = AssetDatabase.GetAssetPath(Selection.activeObject);
 
+        FindImportSettings(path); //Just testing 
+
         AssetImporter importer = AssetImporter.GetAtPath(path);
 
         TextureImporter textureImporter;
@@ -32,5 +34,33 @@ public class AssetImporterTool : MonoBehaviour
         }
 
         AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+    }
+
+    static void FindImportSettings(string pathToSearch)
+    {
+        string[] importSettingsGUIDs = AssetDatabase.FindAssets("t:ImportSettings", new[] { pathToSearch });
+
+        if (importSettingsGUIDs.Length == 0)
+        {
+            //We've reached the main Assets folder. Stop searching and warn theuser that no settings wered found
+            if (pathToSearch == "Assets")
+            {
+                Debug.LogWarning("No import settings found. Use Right Click > Create > Import Settings to create some");
+            }
+            else
+            {
+                pathToSearch = System.IO.Directory.GetParent(pathToSearch).ToString();
+                FindImportSettings(pathToSearch);
+            }
+        }
+        else //We found an ImportSettings object to use
+        {
+            foreach (string guid in importSettingsGUIDs)
+            {
+                string settingsPath = AssetDatabase.GUIDToAssetPath(guid);
+                ImportSettings settingsObject = AssetDatabase.LoadAssetAtPath<ImportSettings>(settingsPath);
+                Debug.Log($"Found settings: {settingsPath}");
+            }
+        }
     }
 }
