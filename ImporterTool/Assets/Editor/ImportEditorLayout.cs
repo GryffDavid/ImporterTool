601fac903d83d84870342b060fd29d1445068625
Gryff David <gryffdavid@gmail.com>
2021-05-09 15:50:12 +0200

    Created a custom editor layout called "ImportEditorLayout". It makes for a more polished version of the tool, but I do think that it makes it far less easy to extend and more difficult to build upon in the future. Having to manually layout the editor GUI takes away from the ability to very quickly add new settings to the ImportSettings. It could easily be disabled and have the default layout used.

---
+++ ImporterTool/Assets/Editor/ImportEditorLayout.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ImportSettings))]
+public class ImportEditorLayout : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ImportSettings settings = target as ImportSettings;
+
+        #region Audio import settings custom inspector layout
+        EditorGUILayout.LabelField("Audio Settings", EditorStyles.boldLabel);
+        settings.AudioSampleRate = (AudioSampleRateSetting)EditorGUILayout.EnumPopup("Audio Sample Rate", settings.AudioSampleRate);
+        EditorGUI.BeginDisabledGroup(!(settings.AudioSampleRate == AudioSampleRateSetting.OverrideSampleRate));
+        settings.SampleRateOverride = (ImportSettings.SampleRateOverrideEnum)EditorGUILayout.EnumPopup("Audio Sample Rate Override", settings.SampleRateOverride);
+        EditorGUI.EndDisabledGroup();
+        settings.CompressionFormat = (AudioCompressionFormat)EditorGUILayout.EnumPopup("Audio Compression Format", settings.CompressionFormat);
+        settings.AudioLoadType = (AudioClipLoadType)EditorGUILayout.EnumPopup("Audio Clip Load Type", settings.AudioLoadType);
+
+        #region Android Audio Settings
+        GUILayout.Space(10);
+        settings.OverrideAndroidAudioSettings = GUILayout.Toggle(settings.OverrideAndroidAudioSettings, "Override Audio Settings for Android");
+        EditorGUI.BeginDisabledGroup(!settings.OverrideAndroidAudioSettings);
+        settings.AndroidAudioSampleRate = (AudioSampleRateSetting)EditorGUILayout.EnumPopup("Audio Sample Rate", settings.AndroidAudioSampleRate);
+        EditorGUI.BeginDisabledGroup(!(settings.AndroidAudioSampleRate == AudioSampleRateSetting.OverrideSampleRate));
+        settings.AndroidSampleRateOverride = (ImportSettings.SampleRateOverrideEnum)EditorGUILayout.EnumPopup("Audio Sample Rate Override", settings.AndroidSampleRateOverride);
+        EditorGUI.EndDisabledGroup();
+        settings.AndroidCompressionFormat = (AudioCompressionFormat)EditorGUILayout.EnumPopup("Audio Compression Format", settings.AndroidCompressionFormat);
+        settings.AndroidAudioClipLoadType = (AudioClipLoadType)EditorGUILayout.EnumPopup("Audio Clip Load Type", settings.AndroidAudioClipLoadType);
+        EditorGUI.EndDisabledGroup();
+        #endregion 
+        #endregion
+
+        GUILayout.Space(10);
+
+        #region Texture import settings custom inspector layout
+        EditorGUILayout.LabelField("Texture Settings", EditorStyles.boldLabel);
+        settings.MaxTextureSize = (ImportSettings.MaxTextureSizeEnum)EditorGUILayout.EnumPopup("Max Texture Size", settings.MaxTextureSize);
+        settings.TetxureFilterMode = (FilterMode)EditorGUILayout.EnumPopup("Filter Mode", settings.TetxureFilterMode);
+
+        EditorGUI.BeginDisabledGroup(settings.TetxureFilterMode == FilterMode.Point);
+        settings.FilterLevel = EditorGUILayout.IntSlider("Anisotropic Filtering Level", settings.FilterLevel, 0, 16);
+        EditorGUI.EndDisabledGroup();
+
+        #region Android Texture Settings
+        GUILayout.Space(10);
+        settings.OverrideAndroidTextureSettings = GUILayout.Toggle(settings.OverrideAndroidTextureSettings, "Override Texture Settings for Android");
+        EditorGUI.BeginDisabledGroup(!settings.OverrideAndroidTextureSettings);
+        settings.AndroidMaxTextureSize = (ImportSettings.MaxTextureSizeEnum)EditorGUILayout.EnumPopup("Max Android Texture Size", settings.AndroidMaxTextureSize);
+        EditorGUI.EndDisabledGroup();
+        #endregion 
+        #endregion
+
+        //base.OnInspectorGUI();
+    }
+}
